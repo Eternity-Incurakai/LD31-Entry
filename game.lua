51b19a175b90b27437d52b95b012d30f6aef0c65
iggyvolz <iggyvolz@gmail.com>
2014-12-06 16:03:57 -0500

    Spawn based on ads spent

--- game.lua
+++ game.lua
@@ -142,7 +142,7 @@ function self.go()
       self.people[#self.people].x=math.random(780)
       self.people[#self.people].y=math.random(530)
     end
-    self.nextspawntime=3+self.time
+    self.nextspawntime=2/(cf+0.1)+self.time
   end
   function love.draw()
     love.graphics.setFont(font(12))
