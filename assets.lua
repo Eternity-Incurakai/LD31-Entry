5b3b26ea805415b64e1ff2379fcb51c498ea2bcd
iggyvolz <iggyvolz@gmail.com>
2014-12-05 23:19:07 -0500

    Add person image

--- assets.lua
+++ assets.lua
@@ -3,6 +3,7 @@ local self={
     ["eilogo"] = love.graphics.newImage("assets/eilogo.png"),
     ["love"] = love.graphics.newImage("assets/love.png"),
     ["lua"] = love.graphics.newImage("assets/lua.png"),
-    ["splashmusic"] = love.audio.newSource("assets/splashmusic.mp3","static"),
+    ["person"] = love.graphics.newImage("assets/person.png"),
+    ["splashmusic"] = love.audio.newSource("assets/splashmusic.mp3","static")
 }
 return self
